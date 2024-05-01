93ce11740f9cf445f5ea8971925dd00cda627fe8
Ernazar dev <maksetovernazar828@gmail.com>
2024-05-01 22:39:56 +0500

    dynamic title

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,25 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/login', (req, res) => {
-	res.render('login')
+	res.render('login', {
+		title: 'Login | Boom Shop',
+		isLogin: true
+	})
 })
 router.get('/register', (req, res) => {
-	res.render('register')
+	res.render('register', {
+		title: 'Register | Boom Shop',
+		isRegister: true
+	})
 })
 
+router.post('/login',(req, res) => {
+	console.log(req.body);
+	res.redirect('/')
+})
+
+router.post('/register', (req, res) => {
+	console.log(req.body)
+	res.redirect('/')
+})
 export default router
--- routes/products.js
+++ routes/products.js
@@ -2,13 +2,21 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-	res.render('index')
+	res.render('index', {
+		title: 'Main | Boom Shop',
+	})
 })
 router.get('/products', (req, res) => {
-	res.render('products')
+	res.render('products', {
+		title: 'Products | Boom Shop',
+		isProducts: true
+	})
 })
 router.get('/add', (req, res) => {
-	res.render('add')
+	res.render('add', {
+		title: 'Add products | Boom Shop',
+		isAdd: true
+	})
 })
 
 export default router
