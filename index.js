0e24933e8a441880d5925dac760aac0a06b7a117
Ernazar dev <maksetovernazar828@gmail.com>
2024-05-01 10:26:49 +0500

    configuration of handlebars and creating partials

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express";
-import path, { dirname } from "path"
-import { fileURLToPath } from "url"
-import { engine } from "express-handlebars"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import { engine, create } from "express-handlebars"
 
 const app = express()
+const hbs = create({defaultLayout: "main", extname: "hbs",})
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
