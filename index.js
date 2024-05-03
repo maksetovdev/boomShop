eb63c9a0b8c91aeb2bf7d1558375bafb10bf293d
Ernazar dev <maksetovernazar828@gmail.com>
2024-05-03 10:09:44 +0500

    static folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const hbs = create({defaultLayout: "main", extname: "hbs",})
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
+app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 
 //Routes
