568b8f46a5cb2d1325c808bac5ea330f3beb1567
Ernazar dev <maksetovernazar828@gmail.com>
2024-05-05 18:58:33 +0500

    user login and refactoring

--- routes/auth.js
+++ routes/auth.js
@@ -17,12 +17,25 @@ router.get('/register', (req, res) => {
 	})
 })
 
-router.post('/login',(req, res) => {
-	console.log(req.body);
+router.post('/login', async (req, res) => {
+
+	const existUser = await userModel.findOne({email: req.body.email})
+	if(!existUser) {
+	console.log('User not found!');
+	return
+	}	
+
+	const isPassEqual = await bcrypt.compare(req.body.password, existUser.password)
+	if(!isPassEqual) {
+		console.log('Password was wrong!');
+		return
+	}
+
 	res.redirect('/')
 })
 
 router.post('/register', async (req, res) => {
+
 	const hashedPassword = await bcrypt.hash(req.body.password, 10)
 	const userData = {
 		firstName: req.body.firstName,
@@ -30,6 +43,7 @@ router.post('/register', async (req, res) => {
 		email: req.body.email,
 		password: hashedPassword,
 	}
+	
 	const user = await userModel.create(userData)
 	res.redirect('/')
 })
